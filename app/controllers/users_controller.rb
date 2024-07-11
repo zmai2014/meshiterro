8396455fbc28c602b1db618bc9b7f1e453e3a600
zmai2014 <170424485+zmai2014@users.noreply.github.com>
2024-07-11 14:04:48 +0000

    [finish]24+2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_maching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -19,5 +20,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+  def is_match_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_image_path
+    end
+  end
 end
